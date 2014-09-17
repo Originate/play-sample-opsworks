7627418ad8a5e2eb67147de101ddd556c89a6c73
Maxime Bury <maxime.bury@gmail.com>
2014-09-17 15:14:27 -0400

    Initialize cookbook

---
+++ app/metadata.rb
@@ -0,0 +1,8 @@
+name             'app'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures app'
+long_description 'Installs/Configures app'
+version          '0.1.0'
+
